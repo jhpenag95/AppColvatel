9cb13189e79a2a58339f002c562fdff3d905af55
Johan Gordillo <helver248@hotmail.es>
2023-02-05 19:42:44 -0500

    se modifica vab de nav_inicio.php

--- views/nav_inicio.php
+++ views/nav_inicio.php
@@ -1,8 +1,9 @@
 <header class="p-3 text-bg-dark">
     <div class="container">
       <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-        <a href="inicio.php" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-            <img src="../assets/icons/icono-tipo-plan.gif" width="200" height="auto" class="bi me-2" role="img" alt="logo">
+        <a href="inicio.php" class="d-flex align-items-center justify-content-center mb-2 mb-lg-0 text-white text-decoration-none fs-4 mx-4">
+            <!-- <img src="../assets/icons/icono-tipo-plan.gif" width="200" height="auto" class="bi me-2" role="img" alt="logo"> -->
+            Hogar Digital
         </a>
 
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
